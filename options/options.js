d1f7ef6cf747d6de4456c932f9d2634125ebbe00
Crowdbotics <team@crowdbotics.com>
2023-10-16 14:50:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-bush-43966",
+  url: "https://broken-bush-43966.botics.co",
+  api: "https://broken-bush-43966.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
